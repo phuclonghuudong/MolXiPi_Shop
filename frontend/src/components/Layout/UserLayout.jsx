1d399c0f948ecdbd661b481159cfdd0becf2d877
phuc <phucpham.241099@gmail.com>
2025-11-13 21:17:50 +0700

    footer & setup main content - FE

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,9 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Common/Footer";
 import Header from "../Common/Header";
 
 const UserLayout = () => {
   return (
     <div>
       <Header />
+      {/* Main Content  */}
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 };
